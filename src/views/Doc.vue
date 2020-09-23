47c57575f2a96fc760c60f75b3949ab7006f20c7
ricky L <RickyLei7@Gmail.com>
2020-09-22 23:19:28 -0600

    done HTML

---
+++ src/views/Doc.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="topNav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="content">
+      <aside>sideBar</aside>
+      <main>main</main>
+
+    </div>
+
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="topNav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>Drwing UI framework</h1>
+      <h2>A very Lihai UI framework</h2>
+      <p class="actions">
+        <a href="">Github</a>
+        <a href="">Begin</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .topNav
+</style>
