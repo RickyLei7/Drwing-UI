3d8c663b2d9186be716c095cf039dabcbdbcc3e9
ricky L <RickyLei7@Gmail.com>
2020-10-04 22:33:30 -0600

    after hover white circle go left

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    Switch doc
-
+    <Switch />
   </div>
 </template>
+
+<script lang="ts">
+  import Switch from '../lib/Switch.vue'
+  export default {
+    components: {Switch}
+  }
+
+</script>
